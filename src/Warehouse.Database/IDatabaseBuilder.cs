fc9597b60007fb56b57ed1a024b5d69d72995ce7
Anton Kuz'menko <compradon@icloud.com>
2020-10-29 09:39:38 +0300

    Add IDatabaseBuilder

--- src/Warehouse.Database/IDatabaseBuilder.cs
+++ src/Warehouse.Database/IDatabaseBuilder.cs
@@ -1,10 +1,30 @@
+using System.Threading.Tasks;
+
 namespace Compradon.Warehouse.Database
 {
     /// <summary>
-    /// Provides an abstraction for building a database schema.
+    /// Provides an abstraction for building a warehouse database schema.
     /// </summary>
     public interface IDatabaseBuilder
     {
+        /// <summary>
+        /// Creates the warehouse database schema.
+        /// </summary>
+        Task CreateAsync();
 
+        /// <summary>
+        /// Drops the warehouse database schema.
+        /// </summary>
+        Task DropAsync();
+
+        /// <summary>
+        /// Determines whether the specified warehouse database schema exists.
+        /// </summary>
+        Task<bool> ExistsAsync();
+
+        /// <summary>
+        /// Gets the version of the warehouse database schema.
+        /// </summary>
+        Task<string> VersionAsync();
     }
 }
