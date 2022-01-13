3ca10c1b759f112b116c4a5a0d5bfbfb267bb023
Anton Kuz'menko <compradon@icloud.com>
2022-01-13 12:47:06 +0300

    Modify DataReaderExtensions class
    * Refactors

--- src/Warehouse.Database/DataReader.cs
+++ src/Warehouse.Database/DataReader.cs
@@ -1,4 +1,6 @@
 using System.Data;
+using System.Text;
+using System.Text.Json;
 
 namespace Compradon.Warehouse.Database
 {
@@ -8,12 +10,26 @@ namespace Compradon.Warehouse.Database
     public static class DataReaderExtensions
     {
         /// <summary>
+        /// Reads the string value from the <see cref="IDataReader"/>.
+        /// </summary>
+        public static string ReadString(this IDataReader dataReader)
+        {
+            var builder = new StringBuilder();
+            while (dataReader.Read()) builder.Append(dataReader.GetString(0));
+
+            return builder.ToString();
+        }
+
+        /// <summary>
         /// Creates and binds an instance from the <see cref="IDataReader"/>.
         /// </summary>
         /// <typeparam name="T">The type to return.</typeparam>
-        public static T Build<T>(this IDataReader dataReader)
+        public static T Build<T>(this IDataReader dataReader) where T : class
         {
-            return default(T);
+            var value = dataReader.ReadString();
+            if (value.Length == 0) return null;
+
+            return JsonSerializer.Deserialize<T>(value);
         }
     }
 }
